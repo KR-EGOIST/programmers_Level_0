ff20f98a994d67f49f433af2512ed408afd976df
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-04-08 17:58:58 +0900

    문자열 돌리기

---
+++ 2일차/문자열 돌리기.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input = [line];
+}).on('close', function () {
+  str = input[0].split('');
+
+  str.forEach((i) => {
+    console.log(i);
+  });
+
+  // [...str].forEach(c => console.log(c))
+});
