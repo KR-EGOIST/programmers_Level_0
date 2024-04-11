0d8b328f4367bc77e858283353225c6c26d7b540
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-04-11 15:14:11 +0900

    간단한 논리 연산

---
+++ 8일차/간단한 논리 연산.js
@@ -0,0 +1,9 @@
+function solution(x1, x2, x3, x4) {
+  let answer = true;
+
+  answer = (x1 || x2) && (x3 || x4);
+  return answer;
+}
+
+let result = solution(false, true, true, true);
+console.log(result);
