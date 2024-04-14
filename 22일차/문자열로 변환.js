d7dc607ed22462389ff5ce850f93831987853657
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-04-14 16:50:24 +0900

    문자열로 변환

---
+++ 22일차/문자열로 변환.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  return String(n);
+}
+
+// function solution(n) {
+//   return n + '';
+//   return n.toString();
+// }
+
+let result = solution(123);
+console.log(result);
