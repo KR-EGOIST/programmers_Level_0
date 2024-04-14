987f993997e9cfae2ef6be6fa99e831d175f24b1
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-04-14 11:27:36 +0900

    왼쪽 오른쪽

---
+++ 13일차/왼쪽 오른쪽.js
@@ -0,0 +1,14 @@
+function solution(str_list) {
+  for (let i = 0; i < str_list.length; i++) {
+    if (str_list[i] == 'l') {
+      return str_list.slice(0, i);
+    }
+    if (str_list[i] == 'r') {
+      return str_list.slice(i + 1);
+    }
+  }
+  return [];
+}
+
+let result = solution(['u', 'u', 'l', 'r']);
+console.log(result);
