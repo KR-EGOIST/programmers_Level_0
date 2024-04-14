928ff08ffa4d8100f54ef0b7e2917d06f1629698
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-04-14 16:37:13 +0900

    정수 부분

---
+++ 21일차/정수 부분.js
@@ -0,0 +1,10 @@
+// Math.ceil() : 소수점 올림, 정수 반환
+// Math.floor() : 소수점 버림, 정수 반환
+// Math.round() : 소수점 반올림, 정수 반환
+
+function solution(flo) {
+  return Math.floor(flo);
+}
+
+let result = solution(1.42);
+console.log(result);
